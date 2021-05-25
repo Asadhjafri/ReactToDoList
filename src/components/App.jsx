22a033ac593cdcc3a8145909ee44a7ee8020f5cd
Asad Jafri <51427124+Asadhjafri@users.noreply.github.com>
2021-05-25 12:39:49 -0400

    add state for text and items for list. Add on click and on change for inputs and button to create functionality to add item to to do list.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  function addToDoItem() {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleChange} type="text" value={inputText} />
+        <button onClick={addToDoItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li> Item #1</li>
+          {items.map((todoItem) => (
+            <li>{todoItem} </li>
+          ))}
         </ul>
       </div>
     </div>
