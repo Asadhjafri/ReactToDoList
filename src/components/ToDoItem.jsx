f117661719785e02c44c7d4223475d0265c492d9
Asad Jafri <51427124+Asadhjafri@users.noreply.github.com>
2021-05-26 13:09:04 -0400

    create crossout functionality in ToDoItem. List item gets crossed out when completed

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ToDoItem(props) {
-  return <li>{props.text}</li>;
+  const [crossOut, setCrossOut] = useState(false);
+
+  function handleCrossOut() {
+    setCrossOut((prevValue) => {
+      return !prevValue;
+    });
+  }
+
+  return (
+    <div onClick={handleCrossOut}>
+      <li style={{ textDecoration: crossOut ? "line-through" : "none" }}>
+        {props.text}
+      </li>
+    </div>
+  );
 }
 
 export default ToDoItem;
